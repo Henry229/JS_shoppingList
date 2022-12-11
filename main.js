96a8d8323a52704efbfbef3b6ab67bd352615cef
Henry Chun <chunhyungjoon@gmail.com>
2022-12-11 19:32:50 +1100

    fix add &U update

--- main.js
+++ main.js
@@ -1,28 +1,46 @@
+const form = document.querySelector('.form');
 const items = document.querySelector('.items');
 const input = document.querySelector('.input');
 const submitBtn = document.querySelector('.submitBtn');
 let toBeEdited = '';
 let update = false;
 
-const addItem = () => {
+const selectOption = (event) => {
+  console.log('-----yogida4', update);
+  if (update) {
+    updateItem(event);
+    update = false;
+  } else {
+    addItem(event);
+  }
+};
+
+const addItem = (event) => {
+  event.preventDefault();
+  console.log(form.value);
+  console.log(input.value);
   const inputValue = input.value;
   if (!inputValue) {
     input.focus();
     return;
   }
+  console.log('####Yogida2', update);
   const item = createItem(inputValue);
   items.appendChild(item);
   input.value = '';
   input.focus();
 };
 
-const updateItem = () => {
+const updateItem = (event) => {
+  event.preventDefault();
   const inputValue = input.value;
   if (!inputValue) {
     input.focus();
     return;
   }
+  console.log('****Yogida1', update);
   toBeEdited.children[0].children[0].innerText = inputValue;
+  update = false;
   input.value = '';
   input.focus();
   toBeEdited = '';
@@ -52,25 +70,44 @@ const createItem = (value) => {
   return itemRow;
 };
 
-submitBtn.addEventListener('click', () => {
-  if (update) {
-    updateItem();
-    update = false;
-  } else {
-    addItem();
-  }
-});
+form.addEventListener('submit', selectOption);
+// update ? updateItem() : addItem
+// update ? console.log('@@@updateItem') : addItem;
+//   updateItem();
+//   update = false;
+// } else {
+//   addItem();
+// }
+// });
+// form.addEventListener('submit', () => {
+//   console.log('****yogida1');
+//   if (update) {
+//     updateItem();
+//     update = false;
+//   } else {
+//     addItem;
+//   }
+// });
 
-input.addEventListener('keyup', (event) => {
-  if (event.key === 'Enter') {
-    if (update) {
-      updateItem();
-      update = false;
-    } else {
-      addItem();
-    }
-  }
-});
+// submitBtn.addEventListener('click', () => {
+//   if (update) {
+//     updateItem();
+//     update = false;
+//   } else {
+//     addItem();
+//   }
+// });
+
+// input.addEventListener('keyup', (event) => {
+//   if (event.key === 'Enter') {
+//     if (update) {
+//       updateItem();
+//       update = false;
+//     } else {
+//       addItem();
+//     }
+//   }
+// });
 
 items.addEventListener('click', (event) => {
   const id = event.target.dataset.del;
@@ -83,6 +120,7 @@ items.addEventListener('click', (event) => {
     toBeEdited = document.querySelector(`.itemRow[data-edit="${jd}"]`);
     span = toBeEdited.children[0].children[0];
     update = true;
+    console.log('### update: ', update);
     submitBtn.innerText = 'update';
     input.value = span.innerText;
     input.innerText = input.value;
